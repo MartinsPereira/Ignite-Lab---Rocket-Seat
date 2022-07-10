e51f16c0024d259c69da9fbe6c1d84d6464d12e7
Martins Pereira <martins.silva@etec.sp.gov.br>
2022-07-10 18:49:53 -0300

    aula 4 completa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import { Logo } from "./Logo";
 
 export function Header() {
+
   return (
     <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
       <Logo />
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o64kdp0ry801xm70ti9r31/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
